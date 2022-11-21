0ae793d56b2959fc5569785ebb3fe67cef3e247e
atharvabe19e <106114931+atharvabe19e@users.noreply.github.com>
2022-11-21 06:55:33 +0530

    Create Students.sol

---
+++ Students.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+contract Database{
+
+    struct student{
+        int ID;
+        string _fname;
+        string _lname;
+    }
+
+    int public Count = 0;
+
+    mapping(int => student) public stdRecords;
+
+    function addNew(int _id, string calldata _fname, string calldata _lname) public{
+                        Count+=1;
+                        stdRecords[Count] = student(_id, _fname, _lname);
+                    }
+}
